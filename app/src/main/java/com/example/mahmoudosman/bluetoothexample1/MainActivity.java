26045efcc041880cbcd6fcde643b21a5da92c533
Mahmoud Osman <mahmoudosman1819@gmail.com>
2019-01-02 16:15:22 +0200

    List of Paire Devices

--- app/src/main/java/com/example/mahmoudosman/bluetoothexample1/MainActivity.java
+++ app/src/main/java/com/example/mahmoudosman/bluetoothexample1/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.mahmoudosman.bluetoothexample1;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     BluetoothAdapter mBluetoothAdapter;
     Intent btEnablingIntent;
     int requestcodeForEnable = 1;
-    Button btnOn, btnOff;
+    Button btnOn, btnOff, btnShowDevices;
+    ListView devicesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
         btnOn = findViewById(R.id.btn_on);
         btnOff = findViewById(R.id.btn_off);
+        btnShowDevices = findViewById(R.id.btn_show_devices);
+        devicesList = findViewById(R.id.list_devices);
+
+
         //TODO(3) init adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         btEnablingIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -31,19 +42,42 @@ public class MainActivity extends AppCompatActivity {
 
         bluetoothOnMethod();
         bluetoothOffMethod();
+        showDevices();
 
+    }
+
+    private void showDevices() {
+        btnShowDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Set<BluetoothDevice> deviceSet = mBluetoothAdapter.getBondedDevices();
+                String[] pairedDevices = new String[deviceSet.size()];
+                int index = 0;
+
+                if (deviceSet.size() > 0) {
+
+                    for (BluetoothDevice BluetoothDevice : deviceSet) {
+                        pairedDevices[index] = BluetoothDevice.getName();
+                        index++;
+                    }
+
+                    ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_expandable_list_item_1,pairedDevices);
+                    devicesList.setAdapter(arrayAdapter);
+                }
+            }
+        });
 
     }
 
     private void bluetoothOffMethod() {
-    btnOff.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (mBluetoothAdapter.isEnabled()){
-                mBluetoothAdapter.disable();
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBluetoothAdapter.isEnabled()) {
+                    mBluetoothAdapter.disable();
+                }
             }
-        }
-    });
+        });
     }
 
 
@@ -52,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == requestcodeForEnable) {
             if (requestCode == RESULT_OK) {
                 Toast.makeText(getApplicationContext(), "Bluetooth is Enable", Toast.LENGTH_LONG).show();
-            }else if (requestCode==RESULT_CANCELED){
+            } else if (requestCode == RESULT_CANCELED) {
                 Toast.makeText(getApplicationContext(), "Bluetooth is Enabling Cancell", Toast.LENGTH_LONG).show();
             }
         }
